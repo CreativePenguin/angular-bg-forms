c2bf6d03593534965805571f59250b0b1103b673
Winston <panda501wp@gmail.com>
2024-10-08 03:01:05 -0400

    add autocomplete feature, but it is not properly filtering the list as the input flows in

--- src/app/attack-rolls/attack-rolls.component.ts
+++ src/app/attack-rolls/attack-rolls.component.ts
@@ -1,20 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { SpellsService } from '../spells.service';
-import { Observable } from 'rxjs';
+import { map, Observable, startWith } from 'rxjs';
 import { Spell, SpellResponse, SpellResponseResults } from '../spell';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { DropdownSearchComponent } from "../dropdown-search/dropdown-search.component";
 import { DropdownItem } from '../dropdown';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-attack-rolls',
   standalone: true,
   imports: [
     CommonModule, MatInputModule, ReactiveFormsModule,
-    MatSelectModule,
+    MatSelectModule, MatAutocompleteModule,
     DropdownSearchComponent
 ],
   templateUrl: './attack-rolls.component.html',
@@ -24,7 +25,9 @@ export class AttackRollsComponent implements OnInit{
   spellsService: SpellsService = inject(SpellsService);
   rawSpellList$!: Observable<SpellResponse>;
   attackRollsForm = new FormGroup({
-    spell: new FormControl(''),
+    spell: new FormControl<SpellResponseResults>(
+      {index: '-1', level: -1, name: '', url: ''}
+    ),
     spellLevel: new FormControl(1)
   })
   currentSpellRange: number[] = new Array(6);
@@ -48,6 +51,32 @@ export class AttackRollsComponent implements OnInit{
     ]
   };
 
+  adjustLevelFormValues() {
+    this.attackRollsForm.get('spell')?.valueChanges.subscribe(
+      (selectedValue) => {
+        console.log('found change to spell', selectedValue);
+        this.spellsService.getSpell(selectedValue?.url || '').subscribe(
+          (spell) => {
+            if(spell.level == 0) this.currentSpellRange = [0];
+            else this.currentSpellRange = [...Array(7).keys()].slice(spell.level);
+          }
+        )
+      }
+    )
+  }
+
+  filterSpellAutocomplete() {
+    this.attackRollsForm.get('spell')?.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+
+      })
+    )
+  }
+
+  displaySpellAutocompleteValue(selectedValue: SpellResponseResults): string {
+    return selectedValue && selectedValue.name ? selectedValue.name : '';
+  }
 
   constructor() { }
 
@@ -59,17 +88,7 @@ export class AttackRollsComponent implements OnInit{
       this.spellsOfEachLevel$[`level ${i}`] = this.spellsService.getAllSpellsOfLevel(i);
       // this.spellsOfEachLevel$.set(`level ${i}`, this.spellsService.getAllSpellsOfLevel(i));
     }
-    this.attackRollsForm.get('spell')?.valueChanges.subscribe(
-      (selectedValue) => {
-        console.log('found change to spell', selectedValue);
-        this.spellsService.getSpell(selectedValue || '').subscribe(
-          (spell) => {
-            if(spell.level == 0) this.currentSpellRange = [0];
-            else this.currentSpellRange = [...Array(7).keys()].slice(spell.level);
-          }
-        )
-      }
-    )
+    this.adjustLevelFormValues();
   }
 
 }
