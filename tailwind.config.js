d0d50bf6ac562cb86b99afced25f79e8c8c8d9f6
Winston <panda501wp@gmail.com>
2024-10-26 07:29:09 -0400

    routing now works, modified tailwind config to also allow use in base app folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{html, js}"
+    "./src/app/**/*.{html, js}",
+    "./src/app/*.{html, js}"
   ],
   theme: {
     colors: {
