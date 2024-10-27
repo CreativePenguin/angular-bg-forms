c346407a5d1d303d696b20b62b7398123a8c2343
Winston <panda501wp@gmail.com>
2024-10-26 20:14:17 -0400

    add new not-found page, and sidebar nav. Will need to add stylings next

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,18 @@ import { NavbarComponent } from "./navbar/navbar.component";
 import { OverlayComponent } from './overlay/overlay.component';
 import { AttackRollsComponent } from './attack-rolls/attack-rolls.component';
 import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet, NavbarComponent, CommonModule, RouterLink, RouterLinkActive,
-    SkillCheckComponent, RouterModule, OverlayComponent
+    SkillCheckComponent, RouterModule, OverlayComponent, MatSidenavModule,
+    MatToolbarModule, MatButtonModule, MatIconModule
   ],
   // template: ``,
   templateUrl: './app.component.html',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { SkillCheckComponent } from './skill-check/skill-check.component';
 import { AttackRollsComponent } from './attack-rolls/attack-rolls.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-    {path: '', component: SkillCheckComponent, title: 'Skill Check'},
-    {path: 'attack-roll', component: AttackRollsComponent, title: 'Attack Rolls'}
+    {path: 'skill-check', component: SkillCheckComponent, title: 'Skill Check'},
+    {path: 'attack-roll', component: AttackRollsComponent, title: 'Attack Rolls'},
+    {path: '', redirectTo: '/skill-check', pathMatch: 'full'},
+    {path: '**', component: NotFoundComponent},
 ];
