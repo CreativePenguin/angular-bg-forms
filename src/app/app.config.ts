245415add2888c01d40e97bc05fa57893db9d1df
Winston <panda501wp@gmail.com>
2024-11-17 04:58:39 -0500

    deployed successfully for once. Testing gh actions to see if that helps

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,6 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), provideFirebaseApp(() => initializeApp({"projectId":"test-00-c77de","appId":"1:864287131419:web:4b60183cca62591db7b5be","storageBucket":"test-00-c77de.firebasestorage.app","apiKey":"AIzaSyDP4kd1doMBPfOSirbE7I6DYLjMbbNbTAY","authDomain":"test-00-c77de.firebaseapp.com","messagingSenderId":"864287131419"})), provideAuth(() => getAuth())
+    provideRouter(routes), provideAnimationsAsync(), provideHttpClient(), provideFirebaseApp(() => initializeApp({"projectId":"test-00-c77de","appId":"1:864287131419:web:4b60183cca62591db7b5be","storageBucket":"test-00-c77de.firebasestorage.app","apiKey":"AIzaSyDP4kd1doMBPfOSirbE7I6DYLjMbbNbTAY","authDomain":"test-00-c77de.firebaseapp.com","messagingSenderId":"864287131419"})), provideAuth(() => getAuth()), provideFirebaseApp(() => initializeApp({"projectId":"test-00-c77de","appId":"1:864287131419:web:4b60183cca62591db7b5be","storageBucket":"test-00-c77de.firebasestorage.app","apiKey":"AIzaSyDP4kd1doMBPfOSirbE7I6DYLjMbbNbTAY","authDomain":"test-00-c77de.firebaseapp.com","messagingSenderId":"864287131419"})), provideAuth(() => getAuth())
   ]
 };
